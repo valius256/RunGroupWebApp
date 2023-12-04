8458dbfad057960de421f904d4afccb69348282d
valius256 <quangphat7a1@gmail.com>
2023-12-04 15:38:56 +0700

    update repository and their func

---
+++ Repository/ClubRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Club club)
+        {
+            // what happen in here is when we use _context.Add(club); it generating the sql query and when we use return Save() it execute that query
+            _context.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Remove(club);
+        }
+
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsync(int id)
+        {
+            return await _context.Clubs.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Club club)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
